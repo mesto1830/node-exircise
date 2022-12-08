eda192ad378577ec32d4469613d4def6bd412918
mesto1830 <mesto1830@outlook.com>
2022-12-08 21:38:31 +0300

    mm

--- sokcetio4/server.js
+++ sokcetio4/server.js
@@ -1,12 +1,22 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const http = require('http')
+import http from 'http'
 const server = http.createServer(app)
-const { Server } = require("socket.io")
+import { Server } from "socket.io"
 const io = new Server(server)
+import cors from 'cors'
+import helmet from 'helmet'
+import dotenv from 'dotenv'
+
+app.use(cors())
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('static'))
+dotenv.config()
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile('index.html')
 })
 
 
@@ -22,6 +32,6 @@ io.on('connection', (socket) => {
   })
 })
 
-server.listen(4000, () => {
+server.listen(process.env.PORT, () => {
   console.log('listening on *:3000')
 })
