46bc8ed0c949a7e9d3a54e2bb4050f144c5789c3
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-12-12 20:51:43 +0300

    mk

--- child-process/spawn/mongo.js
+++ child-process/spawn/mongo.js
@@ -1,18 +1,16 @@
-const express = require('express')
-const app = express()
 const { spawn } = require('child_process')
-const path = require('path')
 const cron = require('node-cron')
 
-// cron.schedule('*/5 * * * * *', () => backupMongo())
+cron.schedule('*/5 * * * * *', () => backupMongo())
 
 function backupMongo() {
+  //backup
   const child = spawn('mongodump', [
-    '--db=' + 'gasvet',
+    '--db=' + 'works',
     '--host=' + 'localhost'
   ])
+  //show result info
   child.on('exit', (code, signal) => {
-    __dirname + '/gasvet'
     if (code)
       console.log('Backup process exited with code ', code);
     else if (signal)
@@ -22,7 +20,3 @@ function backupMongo() {
   })
 }
 backupMongo()
-
-app.listen(3000, () => {
-  console.log('Server is running...' + process.pid)
-})
